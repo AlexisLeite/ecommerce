fab26765334280a115a2e7c588428c441157e0fe
Alexis Leite <alexisleite.uy@gmail.com>
2025-05-11 14:53:23 -0300

    fix: sass warnings

--- src/app/ecommerce/next.config.ts
+++ src/app/ecommerce/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "../../../node_modules/next/index.js";
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: ["./src/styles"],
+    quietDeps: true,
+    silenceDeprecations: ["import", "legacy-js-api"],
   },
 };
 
